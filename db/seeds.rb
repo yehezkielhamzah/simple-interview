e8adac430ee8e839d320a8bf9e5282ce0c247273
yehezkielhamzah <hamzah.yehezkiel@gmail.com>
2022-08-05 20:43:19 -0700

    partial requirement 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,36 @@
 # - Each doctor should have 10 patients with unique names
 # - Each patient should have 10 appointments (5 in the past, 5 in the future)
 #   - Each appointment should be 50 minutes in duration
+Doctor.create!([{
+    name: 'Dennis Woo'
+},
+{
+    name: 'Edwin Budiman'
+},
+{
+    name: 'Wayne Choi'
+},
+{
+    name: 'Darnaby Lyons'
+},
+{
+    name: 'Victor Wong'
+},
+{
+    name: 'Joseph Baxter'
+},
+{
+    name: 'William Layne'
+},
+{
+    name: 'Richard Park'
+},
+{
+    name: 'Gary White'
+},
+{
+    name: 'Walter Wins'
+},
+])
+
+p 'Created #{Doctor.count} movies'
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    firstName { "MyString" }
+    lastName { "MyString" }
+  end
+end
